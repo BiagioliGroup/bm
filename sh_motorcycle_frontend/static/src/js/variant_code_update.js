1bea9224bf747f773c8cf1e02fc9e83ea583aed2
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-03-30 22:34:31 -0300

    Correccion de JS y de vieews. Se agrega Assets

--- sh_motorcycle_frontend/static/src/js/variant_code_update.js
+++ sh_motorcycle_frontend/static/src/js/variant_code_update.js
@@ -1,38 +1,58 @@
 odoo.define("sh_motorcycle_frontend.variant_code_update", function (require) {
   "use strict";
 
+  var publicWidget = require("web.public.widget");
+  var rpc = require("web.rpc");
+
   console.log("[variant_code_update] Script loaded");
 
-  $(document).ready(function () {
-    console.log("[variant_code_update] Document ready");
+  publicWidget.registry.VariantCodeUpdater = publicWidget.Widget.extend({
+    selector: ".oe_website_sale",
+    start: function () {
+      console.log("[variant_code_update] Widget start");
 
-    $("body").on("change", ".product_id", function () {
-      const productId = $(this).val();
-      console.log("[variant_code_update] product_id changed:", productId);
+      this._updateDefaultCode();
+      this._bindEvents();
+      return this._super.apply(this, arguments);
+    },
+    _bindEvents: function () {
+      var self = this;
+      this.$el.on("change", 'input[name="product_id"]', function () {
+        self._updateDefaultCode();
+      });
+    },
+    _updateDefaultCode: function () {
+      var self = this;
+      var $productInput = this.$('input[name="product_id"]');
+      var $defaultCodeContainer = $("#variant_default_code");
 
-      if (productId) {
-        const $defaultCodeDiv = $("#variant_default_code");
-
-        odoo
-          .rpc({
-            model: "product.product",
-            method: "read",
-            args: [[parseInt(productId)], ["default_code"]],
-          })
-          .then(function (result) {
-            console.log("[variant_code_update] RPC result:", result);
-            if (result && result[0] && result[0].default_code) {
-              $defaultCodeDiv.html(
-                `<strong>Código:</strong> ${result[0].default_code}`
-              );
-            } else {
-              $defaultCodeDiv.html("");
-            }
-          })
-          .catch(function (error) {
-            console.error("[variant_code_update] RPC error:", error);
-          });
+      var productId = parseInt($productInput.val());
+      if (!productId || !$defaultCodeContainer.length) {
+        return;
       }
-    });
+
+      rpc
+        .query({
+          model: "product.product",
+          method: "read",
+          args: [[productId], ["default_code"]],
+        })
+        .then(function (result) {
+          if (result.length && result[0].default_code) {
+            $defaultCodeContainer.html(
+              `<strong>Código:</strong> ${result[0].default_code}`
+            );
+            console.log(
+              "[variant_code_update] Updated code:",
+              result[0].default_code
+            );
+          } else {
+            $defaultCodeContainer.empty();
+          }
+        })
+        .catch(function (error) {
+          console.error("[variant_code_update] RPC error:", error);
+        });
+    },
   });
 });
