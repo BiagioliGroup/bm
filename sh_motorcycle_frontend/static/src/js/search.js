64e299212b63cedffb2ebe5846fc7f5894441645
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-04-27 18:53:52 -0300

    Vuelta atras. Cambiando solo la funcion de "onchangeVehicule"

--- sh_motorcycle_frontend/static/src/js/search.js
+++ sh_motorcycle_frontend/static/src/js/search.js
@@ -8,10 +8,17 @@ import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_d
 publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
   selector: "#wrap",
   events: {
+    // Modal
     "change #id_sh_motorcycle_type_select": "_onChangeTypeGetMake",
     "change #id_sh_motorcycle_make_select": "_onChangeMakeGetYear",
     "change #id_sh_motorcycle_year_select": "_onChangeYearGetModel",
     "change #id_sh_motorcycle_model_select": "_onChangeModel",
+    // Header
+    "change select[name='type']": "_onChangeTypeGetMake",
+    "change select[name='make']": "_onChangeMakeGetYear",
+    "change select[name='year']": "_onChangeYearGetModel",
+    "change select[name='model']": "_onChangeModel",
+    // Botones
     "click #id_sh_motorcycle_select_diff_bike_btn": "_onClickSelectDiffVehicle",
     "click #id_sh_motorcycle_search_diff_bike_close":
       "_onClickSelectDiffVehicleClose",
@@ -22,10 +29,22 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
 
   start: function () {
     var self = this;
-    self.loadTypeList();
+    $("#id_sh_motorcycle_type_select > option").not(":first").remove();
+
+    rpc("/sh_motorcycle/get_type_list").then(function (data) {
+      jQuery.each(data, function (key, value) {
+        $("#id_sh_motorcycle_type_select").append(
+          '<option  value="' + value.id + '">' + value.name + "</option>"
+        );
+      });
+    });
+    $(".id_sh_motorcycle_save_bike_to_garage_btn").css("display");
     self.diable_select_options();
 
+    // when document reload or page refresh
     var result = self.getQueryString();
+
+    // Mostrar el botón solo si hay búsqueda completa
     if (result["type"] && result["make"] && result["year"] && result["model"]) {
       rpc("/sh_motorcycle/is_bike_already_in_garage", {
         type_id: result["type"],
@@ -40,10 +59,14 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
         }
       });
     } else {
+      // Ocultar el botón si falta alguno de los campos
       $("#id_sh_motorcycle_save_bike_to_garage_btn").hide();
     }
 
+    // limpiar links anteriores
     $("#id_sh_motorcycle_select_saved_bike_div > a").remove();
+
+    // cargar links de motos guardadas
     rpc("/sh_motorcycle/get_saved_bike").then(function (data) {
       jQuery.each(data, function (key, value) {
         $("#id_sh_motorcycle_select_saved_bike_div").append(
@@ -57,47 +80,40 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
     });
   },
 
-  loadTypeList: function () {
-    $("#id_sh_motorcycle_type_select > option").not(":first").remove();
-    rpc("/sh_motorcycle/get_type_list").then((data) => {
-      jQuery.each(data, (key, value) => {
-        $("#id_sh_motorcycle_type_select").append(
-          '<option value="' + value.id + '">' + value.name + "</option>"
-        );
-      });
-    });
-  },
-
   _onChangeTypeGetMake: function (e) {
+    var self = this;
     e.preventDefault();
+
+    //clean make
     $("#id_sh_motorcycle_make_select > option").not(":first").remove();
-    $("#id_sh_motorcycle_year_select > option").not(":first").remove();
+
+    //clean model
     $("#id_sh_motorcycle_model_select > option").not(":first").remove();
+
+    //clean year
+    $("#id_sh_motorcycle_year_select > option").not(":first").remove();
+
     rpc("/sh_motorcycle/get_make_list", {
-      type_id:
-        $("#id_sh_motorcycle_type_select").val() ||
-        $('select[name="type"]').val(),
-    }).then((data) => {
-      jQuery.each(data, (key, value) => {
+      type_id: $("#id_sh_motorcycle_type_select > option:selected").val(),
+    }).then(function (data) {
+      jQuery.each(data, function (key, value) {
         $("#id_sh_motorcycle_make_select").append(
           '<option value="' + value.id + '">' + value.name + "</option>"
         );
       });
-      this.diable_select_options();
+      self.diable_select_options();
     });
   },
 
   _onChangeMakeGetYear: function (e) {
     e.preventDefault();
+    // Limpia año y modelo
     $("#id_sh_motorcycle_year_select > option").not(":first").remove();
     $("#id_sh_motorcycle_model_select > option").not(":first").remove();
+    // Carga años para tipo+marca
     rpc("/sh_motorcycle/get_year_list", {
-      type_id:
-        $("#id_sh_motorcycle_type_select").val() ||
-        $('select[name="type"]').val(),
-      make_id:
-        $("#id_sh_motorcycle_make_select").val() ||
-        $('select[name="make"]').val(),
+      type_id: $("#id_sh_motorcycle_type_select").val(),
+      make_id: $("#id_sh_motorcycle_make_select").val(),
     }).then((data) => {
       data.forEach((year) => {
         $("#id_sh_motorcycle_year_select").append(
@@ -110,20 +126,19 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
 
   _onChangeYearGetModel: function (e) {
     e.preventDefault();
+
+    // Limpia modelo
     $("#id_sh_motorcycle_model_select > option").not(":first").remove();
-    $("#no_model_message").remove();
+    $("#no_model_message").remove(); // 🔥 eliminar mensajes anteriores
+
+    // Carga modelos para tipo+marca+año
     rpc("/sh_motorcycle/get_model_list", {
-      type_id:
-        $("#id_sh_motorcycle_type_select").val() ||
-        $('select[name="type"]').val(),
-      make_id:
-        $("#id_sh_motorcycle_make_select").val() ||
-        $('select[name="make"]').val(),
-      year:
-        $("#id_sh_motorcycle_year_select").val() ||
-        $('select[name="year"]').val(),
+      type_id: $("#id_sh_motorcycle_type_select").val(),
+      make_id: $("#id_sh_motorcycle_make_select").val(),
+      year: $("#id_sh_motorcycle_year_select").val(),
     }).then((data) => {
       if (data.length === 0) {
+        // 🔥 Si no hay modelos, mostramos mensaje
         $("#id_sh_motorcycle_model_select").after(
           '<small id="no_model_message" class="text-danger d-block mt-1">No hay modelos disponibles para este año.</small>'
         );
@@ -135,7 +150,13 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
           );
         });
         $("#id_sh_motorcycle_model_select").prop("disabled", false);
+
+        // 🔥 Si hay solo uno, lo seleccionamos automáticamente
+        if (data.length === 1) {
+          $("#id_sh_motorcycle_model_select").val(data[0].id);
+        }
       }
+
       this.diable_select_options();
     });
   },
@@ -193,7 +214,7 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
     self.diable_select_options();
   },
 
-  _onClickSelectDiffVehicleClose: function (ev) {
+  _ctDiffVehicleClose: function (ev) {
     var self = this;
     $("#id_sh_motorcycle_search_diff_bike_div").toggle();
     $("#id_sh_motorcycle_select_diff_bike_btn").toggle();
@@ -215,44 +236,6 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
     return result;
   },
 
-  diable_select_options: function () {
-    var typeSelected = $(
-      "#id_sh_motorcycle_type_select > option:selected"
-    ).val();
-    if (typeSelected) {
-      $("#id_sh_motorcycle_make_select").prop("disabled", false);
-    } else {
-      $("#id_sh_motorcycle_make_select").prop("disabled", true);
-    }
-
-    var makeSelected = $(
-      "#id_sh_motorcycle_make_select > option:selected"
-    ).val();
-    if (makeSelected) {
-      $("#id_sh_motorcycle_year_select").prop("disabled", false);
-    } else {
-      $("#id_sh_motorcycle_year_select").prop("disabled", true);
-    }
-
-    var yearSelected = $(
-      "#id_sh_motorcycle_year_select > option:selected"
-    ).val();
-    if (yearSelected) {
-      $("#id_sh_motorcycle_model_select").prop("disabled", false);
-    } else {
-      $("#id_sh_motorcycle_model_select").prop("disabled", true);
-    }
-
-    var modelSelected = $(
-      "#id_sh_motorcycle_model_select > option:selected"
-    ).val();
-    if (modelSelected) {
-      $("#id_sh_motorcycle_go_submit_button").prop("disabled", false);
-    } else {
-      $("#id_sh_motorcycle_go_submit_button").prop("disabled", true);
-    }
-  },
-
   _onClickSaveBikeToGarage: function (ev) {
     var self = this;
     var result = self.getQueryString();
@@ -263,13 +246,113 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
       model_id: result["model"],
       year: result["year"],
     }).then(function (rec) {
+      // Oculta el botón una vez guardado
       $("#id_sh_motorcycle_save_bike_to_garage_btn").hide();
-      var query = $.param(result);
+
+      // Redirige como si se hubiera hecho clic en la lupa
+      var query = $.param(result); // convierte el objeto en query string
       window.location.href = "/shop?" + query;
     });
   },
 
+  get_param_from_vehicle: function () {
+    var searchParams = new URLSearchParams(window.location.search);
+    var paramType = searchParams.get("type");
+    var paramMake = searchParams.get("make");
+    var paramModel = searchParams.get("model");
+    var paramYear = searchParams.get("year");
+
+    setTimeout(function () {
+      if (paramType > 0) {
+        var selected = $(document)
+          .find(
+            '#id_sh_motorcycle_search_diff_bike_form #id_sh_motorcycle_type_select option[value="' +
+              paramType +
+              '"]'
+          )
+          .attr("selected", "true");
+        if (selected) {
+          var x = $(document)
+            .find(
+              "#id_sh_motorcycle_search_diff_bike_form #id_sh_motorcycle_make_select"
+            )
+            .prop("disabled", false);
+        }
+      }
+      if (paramMake > 0) {
+        $(document)
+          .find(
+            '#id_sh_motorcycle_search_diff_bike_form #id_sh_motorcycle_make_select option[value="' +
+              paramMake +
+              '"]'
+          )
+          .attr("selected", "true");
+      }
+      if (paramModel > 0) {
+        $(document)
+          .find(
+            '#id_sh_motorcycle_search_diff_bike_form #id_sh_motorcycle_model_select option[value="' +
+              paramModel +
+              '"]'
+          )
+          .attr("selected", "true");
+      }
+      if (paramYear > 0) {
+        $(document)
+          .find(
+            '#id_sh_motorcycle_search_diff_bike_form #id_sh_motorcycle_year_select option[value="' +
+              paramYear +
+              '"]'
+          )
+          .attr("selected", "true");
+      }
+    }, 500);
+  },
+
+  diable_select_options: function () {
+    // Marca
+    var typeSelected = $(
+      "#id_sh_motorcycle_type_select > option:selected"
+    ).val();
+    if (typeSelected) {
+      $("#id_sh_motorcycle_make_select").prop("disabled", false);
+    } else {
+      $("#id_sh_motorcycle_make_select").prop("disabled", true);
+    }
+
+    // Año
+    var makeSelected = $(
+      "#id_sh_motorcycle_make_select > option:selected"
+    ).val();
+    if (makeSelected) {
+      $("#id_sh_motorcycle_year_select").prop("disabled", false);
+    } else {
+      $("#id_sh_motorcycle_year_select").prop("disabled", true);
+    }
+
+    // Modelo
+    var yearSelected = $(
+      "#id_sh_motorcycle_year_select > option:selected"
+    ).val();
+    if (yearSelected) {
+      $("#id_sh_motorcycle_model_select").prop("disabled", false);
+    } else {
+      $("#id_sh_motorcycle_model_select").prop("disabled", true);
+    }
+
+    // Botón Ir
+    var modelSelected = $(
+      "#id_sh_motorcycle_model_select > option:selected"
+    ).val();
+    if (modelSelected) {
+      $("#id_sh_motorcycle_go_submit_button").prop("disabled", false);
+    } else {
+      $("#id_sh_motorcycle_go_submit_button").prop("disabled", true);
+    }
+  },
+
   _onClickRemoveVehicle: async function (ev) {
+    var self = this;
     var motorcycle_id = $(ev.currentTarget).data("motorcycle_id");
     this.call("dialog", "add", ConfirmationDialog, {
       body: _t("Estas seguro de eliminar este vehiculo?"),
@@ -280,5 +363,23 @@ publicWidget.registry.sh_motorcycle_shop_search = publicWidget.Widget.extend({
       confirmLabel: _t("Yes"),
       cancel: () => {},
     });
+
+    // new Dialog(this, {
+    //   title: _t("Confirmation"),
+    //   $content: $(
+    //     "<p>" + _t("Are you sure you want to remove the vehicle?") + "</p>"
+    //   ),
+    //   buttons: [
+    //     {
+    //       text: _t("Yes"),
+    //       classes: "btn-primary",
+    //       click: async (ev) => {
+    //         console.log("\n\n motorcycle_id ==", motorcycle_id);
+    //         window.location.href = "/my/garage/remove_bike?id=" + motorcycle_id;
+    //       },
+    //     },
+    //     { text: _t("Cancel"), close: true },
+    //   ],
+    // }).open();
   },
 });
