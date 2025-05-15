93f2e5cf428e18adc8f6313feafc2fbc2f35220c
Sergio Biagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-05-14 21:29:30 -0300

    Correccion en el modelo

--- biagioli_ecom_module/models/models.py
+++ biagioli_ecom_module/models/models.py
@@ -6,8 +6,8 @@ class ProductProduct(models.Model):
     def action_apply_ribbon_by_supplier(self):
         for producto in self:
             supplierinfo = producto.seller_ids[:1]
-            if supplierinfo and supplierinfo.name and producto.inventory_quantity_auto_apply == 0:
-                proveedor = supplierinfo.name
+            if supplierinfo and supplierinfo.partner_id and producto.inventory_quantity_auto_apply == 0:
+                proveedor = supplierinfo.partner_id
                 if proveedor.country_id and proveedor.country_id.name == "Argentina":
                     producto.website_ribbon_id = 6  # 24 hs de demora
                 else:
