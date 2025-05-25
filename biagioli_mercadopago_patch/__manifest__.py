dd8ae18a2316bb60357215b470ebe1a9a387f8f5
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-05-25 15:12:50 -0300

    Correcion inicial de modulo mercadopago

--- biagioli_mercadopago_patch/__manifest__.py
+++ biagioli_mercadopago_patch/__manifest__.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "biagioli_mercadopago_patch",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': 'Biagioli MercadoPago Patch',
+    'version': '1.0.0',
+    'depends': ['payment_mercado_pago'],
+    'author': 'Sergio Biagioli',
+    'category': 'Accounting/Payment Acquirers',
+    'summary': 'Override MercadoPago return URL logic to force HTTPS',
     'description': """
-Long description of module's purpose
+Parche para MercadoPago que reemplaza el armado de URLs de retorno y notificación, forzando HTTPS en lugar de HTTP.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
 
---
+++ biagioli_mercadopago_patch/models/payment_transaction.py
@@ -0,0 +1,50 @@
+from odoo import models
+from werkzeug import urls
+from urllib.parse import quote as url_quote
+from odoo.addons.payment_mercado_pago.controllers.main import MercadoPagoController
+
+
+class PaymentTransaction(models.Model):
+    _inherit = 'payment.transaction'
+
+    def _mercado_pago_prepare_preference_request_payload(self):
+        # Forzar base_url a HTTPS
+        base_url = self.provider_id.get_base_url().replace('http://', 'https://')  # Forzado a HTTPS - Linea agrgado el 25/10/2023.
+        
+
+        # Usar URL de retorno personalizada si está definida, si no usar la default
+        return_url = (self.provider_id.mercadopago_success_url or
+                      urls.url_join(base_url, MercadoPagoController._return_url))
+ 
+        # Sanitizar referencia y formar URL webhook
+        sanitized_reference = url_quote(self.reference)
+        webhook_url = urls.url_join(base_url, f'{MercadoPagoController._webhook_url}/{sanitized_reference}')
+
+        return {
+            'auto_return': 'all',
+            'back_urls': {
+                'success': return_url,
+                'pending': return_url,
+                'failure': return_url,
+            },
+            'external_reference': self.reference,
+            'items': [{
+                'title': self.reference,
+                'quantity': 1,
+                'currency_id': self.currency_id.name,
+                'unit_price': self.amount,
+            }],
+            'notification_url': webhook_url,
+            'payer': {
+                'name': self.partner_name,
+                'email': self.partner_email,
+                'phone': {'number': self.partner_phone},
+                'address': {
+                    'zip_code': self.partner_zip,
+                    'street_name': self.partner_address,
+                },
+            },
+            'payment_methods': {
+                'installments': 1,
+            },
+        }
