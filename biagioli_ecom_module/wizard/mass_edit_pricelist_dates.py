9e8cbf3f2a652483145681a8f6db109b6b498b25
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-06-07 17:37:51 -0300

    CC

--- biagioli_ecom_module/wizard/mass_edit_pricelist_dates.py
+++ biagioli_ecom_module/wizard/mass_edit_pricelist_dates.py
@@ -7,6 +7,23 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
+class MassEditPricelistDates(models.TransientModel):
+    _name = 'mass.edit.pricelist.dates'
+    _description = 'Editor masivo de fechas de listas de precios'
+
+    date_start = fields.Date("Nueva fecha de inicio")
+    date_end = fields.Date("Nueva fecha de finalización")
+
+    def apply_mass_edit(self):
+        active_ids = self.env.context.get('active_ids', [])
+        items = self.env['product.pricelist.item'].browse(active_ids)
+        for item in items:
+            if self.date_start:
+                item.date_start = self.date_start
+            if self.date_end:
+                item.date_end = self.date_end
+
+
 class MassEditPricelistAdjustment(models.TransientModel):
     _name = 'mass.edit.pricelist.adjustment'
     _description = 'Ajuste masivo de precios'
