5fb1f2e69d50d1554161729d1eb4e3407ade0f2b
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-06-07 17:00:55 -0300

    Comentado de Views

--- biagioli_ecom_module/wizard/mass_edit_pricelist_dates.py
+++ biagioli_ecom_module/wizard/mass_edit_pricelist_dates.py
@@ -20,4 +20,35 @@ class MassEditPricelistDates(models.TransientModel):
             if self.date_end:
                 item.date_end = self.date_end
 
-       
+    
+class MassEditPricelistAdjustment(models.TransientModel):
+    _name = 'mass.edit.pricelist.adjustment'
+    _description = 'Ajuste masivo de precios'
+
+    increase_type = fields.Selection([
+        ('percent', 'Porcentaje'),
+        ('fixed', 'Monto fijo')
+    ], string='Tipo de aumento', required=True)
+    value = fields.Float('Valor del aumento', required=True)
+    date_start = fields.Date('Fecha de inicio', required=True)
+
+    def apply_adjustment(self):
+        _logger.info("🛠 Aplicando ajustes de precio")
+
+        active_ids = self.env.context.get('active_ids', [])
+        items = self.env['product.pricelist.item'].browse(active_ids)
+
+        for item in items:
+            if item.compute_price != 'fixed':
+                continue  # ignorar dinámicos
+
+            if self.increase_type == 'percent':
+                new_price = item.fixed_price * (1 + self.value / 100)
+            else:
+                new_price = item.fixed_price + self.value
+
+            item.write({
+                'fixed_price': new_price,
+                'date_start': self.date_start,
+                'date_end': False
+            })
