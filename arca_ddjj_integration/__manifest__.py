9e1ac3357e9274d34ff0f55f3041d453e6103c89
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-06-10 10:38:30 -0300

    INIT de arca_ddjj_integration

---
+++ arca_ddjj_integration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "ARCA DDJJ Integration",
+    "version": "1.0",
+    "depends": ["base", "account"],
+    "author": "Moto Integrale",
+    "category": "Accounting",
+    "summary": "Descarga y cruza comprobantes de ARCA con Odoo para armar la DDJJ",
+    "data": [
+        "views/comprobante_arca_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "application": False,
+}
