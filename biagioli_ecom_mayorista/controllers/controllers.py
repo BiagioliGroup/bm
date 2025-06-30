b822e2757353435aa62f72315366f5a4a36b73d5
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-06-29 23:07:03 -0300

    CC en XMl y controllers

--- biagioli_ecom_mayorista/controllers/controllers.py
+++ biagioli_ecom_mayorista/controllers/controllers.py
@@ -1,22 +1,11 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+from odoo.http import request
 
-
-# class BiagioliEcomModule(http.Controller):
-#     @http.route('/biagioli_ecom_module/biagioli_ecom_module', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/biagioli_ecom_module/biagioli_ecom_module/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('biagioli_ecom_module.listing', {
-#             'root': '/biagioli_ecom_module/biagioli_ecom_module',
-#             'objects': http.request.env['biagioli_ecom_module.biagioli_ecom_module'].search([]),
-#         })
-
-#     @http.route('/biagioli_ecom_module/biagioli_ecom_module/objects/<model("biagioli_ecom_module.biagioli_ecom_module"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('biagioli_ecom_module.object', {
-#             'object': obj
-#         })
-
+@http.route('/', type='http', auth="public", website=True)
+def homepage(**kwargs):
+    show_badge = request.env['ir.config_parameter'].sudo().get_param(
+        'biagioli_ecom_mayorista.show_user_pricelist_badge', default='False'
+    ) == 'True'
+    values = {
+        'show_user_pricelist_badge': show_badge,
+    }
+    return request.render('website.homepage', values)
