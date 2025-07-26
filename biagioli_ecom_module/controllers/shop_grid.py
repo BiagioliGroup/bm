122ae72b28c52e3a47bf47edaf905cf9baa75864
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-07-26 15:37:23 -0300

    Debbuging - controlador y

--- biagioli_ecom_module/controllers/shop_grid.py
+++ biagioli_ecom_module/controllers/shop_grid.py
@@ -12,15 +12,19 @@ _logger = logging.getLogger(__name__)
 class BiagioliWebsiteSale(WebsiteSale):
 
     def _prepare_product_values(self, product, category, search, **kwargs):
-        """
-            Metodo de Sergio Biagioli para mostrar atributos extra en la vista de producto (/shop/product)
-        """
+        _logger.warning("🚨 Sergio DEBUG: Ejecutando _prepare_product_values para %s", product.display_name)
+
         values = super(BiagioliWebsiteSale, self)._prepare_product_values(
             product, category, search, **kwargs)
-
-        values['default_code'] = product.default_code
 
         qty = product.sudo().qty_available
         values['product_qty_display'] = "En stock" if qty > 0 else False
 
+        _logger.warning("🚨 Sergio DEBUG: product_qty_display = %s", values['product_qty_display'])
+
         return values
+    
+    def product(self, product, category='', search='', **kwargs):
+        _logger.warning("🚨 Sergio DEBUG: Entrando al método `product()` de BiagioliWebsiteSale")
+        return super().product(product, category, search, **kwargs)
+
