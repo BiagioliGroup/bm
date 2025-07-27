30d4a5a4d6183d83838eb151033e566682d4626c
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-07-26 21:31:55 -0300

    Actualizacion de compute_depth

--- sh_motorcycle_backend/models/product_category_attribute.py
+++ sh_motorcycle_backend/models/product_category_attribute.py
@@ -23,11 +23,18 @@ class ProductCategory(models.Model):
         string='Atributos Técnicos'
     )
 
-    @api.depends('parent_id', 'parent_id.depth')
+    @api.depends('parent_id')
     def _compute_depth(self):
         for cat in self:
-            cat.depth = (cat.parent_id.depth + 1) if cat.parent_id else 0
+            level = 0
+            parent = cat.parent_id
+            # Sube la cadena de padres hasta llegar a la raíz
+            while parent:
+                level += 1
+                parent = parent.parent_id
+            cat.depth = level
 
+            
 class ProductAttribute(models.Model):
     _inherit = 'product.attribute'
     # Relación inversa para poder hacer domain desde attribute
