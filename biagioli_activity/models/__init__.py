731232f6b43e739151a9e6c139fe33053f47cd91
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-08-20 16:23:27 -0300

    Init en models

--- biagioli_activity/models/__init__.py
+++ biagioli_activity/models/__init__.py
@@ -1,3 +1,4 @@
-# -*- coding: utf-8 -*-
-from . import project_task
-from . import mail_activity_schedule
+
+from . import mail_activity           # 1° Define campos en mail.activity
+from . import project_task           # 2° Extiende project.task  
+from . import mail_activity_schedule # 3° Usa los campos ya definidos
