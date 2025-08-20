92fc8a3e828fc17604ed9db843d096397f15101c
SergioBiagioli <138321404+SergioBiagioli@users.noreply.github.com>
2025-08-20 18:04:40 -0300

    Agregado de notificaciones

--- biagioli_activity/models/__init__.py
+++ biagioli_activity/models/__init__.py
@@ -1,4 +1,5 @@
 
 from . import mail_activity           # 1° Define campos en mail.activity
+from . import activity_notification
 from . import project_task           # 2° Extiende project.task  
 from . import mail_activity_schedule # 3° Usa los campos ya definidos
